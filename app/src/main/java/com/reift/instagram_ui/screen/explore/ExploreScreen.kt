03ac4d7c8eb1493bfc4b7dd5400f91cf1ca3e389
ReihanFatilla <reihanfatilla@gmail.com>
2023-07-28 22:23:29 +0700

    [feat](Explore): finished explore Search bar

--- app/src/main/java/com/reift/instagram_ui/screen/explore/ExploreScreen.kt
+++ app/src/main/java/com/reift/instagram_ui/screen/explore/ExploreScreen.kt
@@ -1,22 +1,48 @@
 package com.reift.instagram_ui.screen.explore
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import com.reift.instagram_ui.screen.home.HomeScreen
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.reift.instagram_ui.model.Post
 import com.reift.instagram_ui.ui.theme.InstagramUITheme
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ExploreScreen(modifier: Modifier) {
-    Column(modifier = modifier) {
-        Box(modifier = modifier, contentAlignment = Alignment.Center) {
-            Text(text = "Explore", textAlign = TextAlign.Center)
+    var query by remember { mutableStateOf("") }
+    LazyColumn(modifier = modifier) {
+        item {
+            TextField(
+                value = query,
+                onValueChange = { query = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.LightGray),
+                leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null )},
+                placeholder = { Text(text = "Search") },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                )
+            )
         }
     }
 }
