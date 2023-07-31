1054659ed845fc8fb15d203a07b5486c9d67510a
ReihanFatilla <reihanfatilla@gmail.com>
2023-07-31 21:23:55 +0700

    [feat](Profile): finished to make Tab and HorizontalPager for post and tag in profile

--- app/src/main/java/com/reift/instagram_ui/screen/profile/TabPagerSection.kt
+++ app/src/main/java/com/reift/instagram_ui/screen/profile/TabPagerSection.kt
@@ -1,6 +1,10 @@
 package com.reift.instagram_ui.screen.profile
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Icon
@@ -12,8 +16,11 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.reift.instagram_ui.screen.profile.screen.MyPostScreen
+import com.reift.instagram_ui.screen.profile.screen.TagScreen
 import com.reift.instagram_ui.ui.theme.InstagramUITheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -23,15 +30,33 @@ import kotlinx.coroutines.launch
 fun TabPagerSection() {
     val pagerState = rememberPagerState(0)
     val scope = rememberCoroutineScope()
-    TabProfile(pagerState, scope)
+    TabProfile(pagerState){ index ->
+        scope.launch {
+            pagerState.animateScrollToPage(index)
+        }
+    }
+    HorizontalPagerProfile(pagerState)
+}
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalPagerProfile(pagerState: PagerState) {
+    HorizontalPager(pageCount = 2, state = pagerState){ index ->
+        val modifier = Modifier.fillMaxSize()
+        Column(modifier = modifier) {
+            when(index){
+                0 -> MyPostScreen(modifier = modifier)
+                1 -> TagScreen(modifier = modifier)
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun TabProfile(
     pagerState: PagerState,
-    scope: CoroutineScope,
+    onClick: (Int) -> Unit,
 ) {
     TabRow(selectedTabIndex = pagerState.currentPage, backgroundColor = Color.White) {
         listOf(Icons.Default.List,
@@ -39,9 +64,7 @@ private fun TabProfile(
             Tab(
                 selected = pagerState.currentPage == index,
                 onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
+                    onClick(index)
                 },
                 icon = {
                     Icon(imageVector = imageVector, contentDescription = null)
