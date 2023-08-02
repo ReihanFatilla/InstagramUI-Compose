703da9a8a3adf0a9a4b3d3092164cd7a0e886826
ReihanFatilla <reihanfatilla@gmail.com>
2023-08-02 17:03:31 +0700

    [fix](Reels): fixed box depth issue

--- app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsOverlay.kt
+++ app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsOverlay.kt
@@ -1,34 +1,21 @@
 package com.reift.instagram_ui.screen.reels
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.MailOutline
-import androidx.compose.material.icons.outlined.Send
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
 import com.reift.instagram_ui.model.Reels
 
 @Composable
-fun ReelsOverlay(modifier: Modifier, reels: Reels){
-    Row(modifier = modifier.padding(bottom = 32.dp)) {
+fun ReelsOverlay(reels: Reels) {
+        Row(modifier = Modifier.background(Color.Transparent).fillMaxSize()) {
         ReelsDetailOverlay(
             reels = reels,
             modifier = Modifier
@@ -41,5 +28,5 @@ fun ReelsOverlay(modifier: Modifier, reels: Reels){
                 .weight(1f)
                 .align(Alignment.Bottom)
         )
-    }
+        }
 }
--- app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsScreen.kt
+++ app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsScreen.kt
@@ -1,36 +1,16 @@
 package com.reift.instagram_ui.screen.reels
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.MailOutline
-import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import coil.compose.rememberAsyncImagePainter
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
-import com.reift.instagram_ui.model.Post
 import com.reift.instagram_ui.model.Reels
-import com.reift.instagram_ui.model.User
 import com.reift.instagram_ui.ui.theme.InstagramUITheme
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -48,41 +28,19 @@ fun ReelsScreen(modifier: Modifier) {
 fun ReelsSection(reels: Reels, modifier: Modifier) {
     Box(modifier = modifier) {
         YoutubeScreen(videoId = reels.youtubeUrl, modifier = modifier)
-        ReelsOverlay(modifier = modifier, reels = reels)
-    }
-}
-
-@Composable
-fun YoutubeScreen(
-    videoId: String,
-    modifier: Modifier,
-) {
-    val ctx = LocalContext.current
-    AndroidView(
-        modifier = modifier,
-        factory = {
-            var view = YouTubePlayerView(it)
-            val fragment = view.addYouTubePlayerListener(
-                object : AbstractYouTubePlayerListener() {
-                    override fun onReady(youTubePlayer: YouTubePlayer) {
-                        super.onReady(youTubePlayer)
-                        youTubePlayer.loadVideo(videoId, 0f)
-                    }
-                }
-            )
-            view
+        Surface(color = Color.Transparent) {
+            ReelsOverlay(reels = reels)
         }
-    )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ReelsPreview() {
     InstagramUITheme {
-        Surface {
-            ReelsScreen(modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black))
-        }
+        ReelsScreen(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black))
+
     }
 }
