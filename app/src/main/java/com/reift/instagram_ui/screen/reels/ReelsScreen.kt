785bf7d3f1427ed4c3667ed20e435bc8721e40e1
ReihanFatilla <reihanfatilla@gmail.com>
2023-08-02 07:21:09 +0700

    [feat](Reels): Finished ReelsOverlay

--- app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsScreen.kt
+++ app/src/main/java/com/reift/instagram_ui/screen/reels/ReelsScreen.kt
@@ -1,24 +1,36 @@
 package com.reift.instagram_ui.screen.reels
 
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import coil.compose.rememberAsyncImagePainter
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import com.reift.instagram_ui.model.Post
 import com.reift.instagram_ui.model.Reels
+import com.reift.instagram_ui.model.User
 import com.reift.instagram_ui.ui.theme.InstagramUITheme
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -36,13 +48,91 @@ fun ReelsScreen(modifier: Modifier) {
 fun ReelsSection(reels: Reels, modifier: Modifier) {
     Box(modifier = modifier) {
         YoutubeScreen(videoId = reels.youtubeUrl, modifier = modifier)
-        ButtonOverlay(reels = reels, modifier = modifier)
+        Row(modifier = modifier.padding(bottom = 32.dp)) {
+            ReelsDetailOverlay(
+                reels = reels,
+                modifier = Modifier
+                    .weight(9f)
+                    .align(Alignment.Bottom)
+            )
+            ReelsButtonOverlay(
+                reels = reels,
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.Bottom)
+            )
+        }
     }
 }
 
 @Composable
-fun ButtonOverlay(reels: Reels, modifier: Modifier) {
-    ReelsTopBar()
+fun ReelsButtonOverlay(reels: Reels, modifier: Modifier) {
+    Column(modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        ButtonColumn(text = reels.likedBy.size.toString(), icon = Icons.Outlined.FavoriteBorder)
+        ButtonColumn(text = reels.listComment.size.toString(), icon = Icons.Outlined.MailOutline)
+        ButtonColumn(text = (10..100).random().toString(), icon = Icons.Outlined.Send)
+    }
+}
+
+@Composable
+private fun ButtonColumn(text: String, icon: ImageVector) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.White
+        )
+        Text(text = text, fontSize = 12.sp, color = Color.White)
+    }
+}
+
+
+@Composable
+fun ReelsDetailOverlay(reels: Reels, modifier: Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Bottom)
+    ) {
+        UserRow(reels)
+        Text(text = reels.caption, fontSize = 12.sp, color = Color.White)
+        LikedByRow(modifier = Modifier , likedBy = reels.likedBy)
+    }
+}
+
+@Composable
+private fun UserRow(reels: Reels) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = reels.user.profileUrl),
+            contentDescription = null,
+            modifier = Modifier
+                .width(25.dp)
+                .aspectRatio(1f)
+                .clip(CircleShape)
+                .background(Color.White)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = reels.user.username,
+            fontSize = 14.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.width(8.dp))
+        OutlinedButton(
+            onClick = { },
+            modifier = Modifier.height(28.dp),
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(1.dp, Color.White),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+        ) {
+            Text(text = "Follow", fontSize = 10.sp, color = Color.White)
+        }
+    }
 }
 
 @Composable
@@ -55,10 +145,15 @@ fun ReelsTopBar() {
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Row(horizontalArrangement = Arrangement.spacedBy(2.dp)) {
-            Text(text = "Reels", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-            Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = null)
+            Text(text = "Reels",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = Color.White)
+            Icon(imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = null,
+                tint = Color.White)
         }
-        Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+        Icon(imageVector = Icons.Default.Menu, contentDescription = null, tint = Color.White)
     }
 }
 
@@ -85,10 +180,44 @@ fun YoutubeScreen(
     )
 }
 
+@Composable
+fun LikedByRow(modifier: Modifier, likedBy: List<User>) {
+    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+        Box(modifier = Modifier.width((20 + (10 * (likedBy.size - 1))).dp)) {
+            likedBy.forEachIndexed { index, story ->
+                Image(
+                    painter = rememberAsyncImagePainter(model = story.profileUrl),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .offset(x = (10 * (index)).dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.White, CircleShape)
+                        .background(Color.White)
+                )
+            }
+        }
+        Text(text = "Liked by",
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp,
+            color = Color.White)
+        Text(text = likedBy.first().username,
+            fontWeight = FontWeight.Bold,
+            fontSize = 12.sp,
+            color = Color.White)
+        Text(text = "and", fontWeight = FontWeight.Normal, fontSize = 12.sp, color = Color.White)
+        Text(text = "others", fontWeight = FontWeight.Bold, fontSize = 12.sp, color = Color.White)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ReelsPreview() {
     InstagramUITheme {
-        ReelsScreen(modifier = Modifier.fillMaxSize())
+        Surface {
+            ReelsScreen(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black))
+        }
     }
 }
